f93291e1205384e329821a918583117333bfd7e8
KaterinaRomanova <44569848+KaterinaRomanova@users.noreply.github.com>
2020-02-26 21:51:01 +0300

    init: start task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,5 +3,5 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
   throw 'Not implemented';
-  // remove line with error and write your code here
+    
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  var count = 0;
+  var matrixRows = [];
+  for(var i = 0; i < matrix.length; i++){
+    var matrixRows = matrix[i];
+    for(var j = 0; j < matrixRows.length; j++){
+      if(matrixRows[j] =="^^"){
+        count++;
+      }
+    }
+  }
+  return count;
 };
