63a4304ca6d97dfc4203e4bd967e882ca115b451
KaterinaRomanova <44569848+KaterinaRomanova@users.noreply.github.com>
2020-03-01 23:00:11 +0300

    feat: added 2 new function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,6 +1,21 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample( /*sampleActivity*/ ) {
+module.exports = function dateSample( sampleActivity) {
+    if(typeof(sampleActivity) != 'string'){
+        return false;
+    }
+    if(isNaN(parseFloat(sampleActivity))){
+        return false;
+    }
+    
+    var sampleActivityNumber = parseFloat(sampleActivity);
 
+    if(sampleActivityNumber >= MODERN_ACTIVITY || sampleActivityNumber <= 0){
+        return false;
+    }else{
+        var age = Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivityNumber)/(0.693/HALF_LIFE_PERIOD));
+    return age;
+    }
 };
+    
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,18 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    if(disksNumber == null || turnsSpeed == null){
+        throw 'Not implemented';
+    }
+    
+    var turns = Math.pow(2,disksNumber); //кол-во оборотов
+    var seconds = turns/(turnsSpeed/3600)-1;
+    var hanoi = {
+        turns: turns,
+        seconds: seconds
+    }
+    
+    return hanoi;
 }
+
+
+
+
