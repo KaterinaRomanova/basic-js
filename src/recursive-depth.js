b1fc8d752c9c63ebb41da6e42b51073a54ad2a81
KaterinaRomanova <44569848+KaterinaRomanova@users.noreply.github.com>
2020-03-02 00:43:26 +0300

    feat: added new function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,5 +1,15 @@
 module.exports = class DepthCalculator {
-    calculateDepth() {
-        
+    calculateDepth(arr) {
+        var max = 1;
+        arr.forEach(element => {
+            if (Array.isArray(element)) {
+                var count = 1;
+                count += this.calculateDepth(element);
+                if (count > max) {
+                    max = count;
+                }
+            }
+        });
+        return max;
     }
 };
